1d870cbfaffaa4ba142b8751cdada952cfec45a7
Micah Njeru <micahnjeru1@gmail.com>
2024-03-25 15:32:04 +0300

    Modified admin, models, views, dockerfile, dockerfile_app, entrypoint, urls, docker-compose. Added serializers & utils repo. Modified the ports and shell file executable bits

--- backend/drf_course/urls.py
+++ backend/drf_course/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from django.contrib import admin
+from core import views as core_views
 from rest_framework import routers
 
-# Rest Framework Router 
+
 router = routers.DefaultRouter()
 
 urlpatterns = router.urls
 
-# Django Default URLs
 urlpatterns += [
     path('admin/', admin.site.urls),
+    path('contact/', core_views.ContactAPIView.as_view()),
 ]
